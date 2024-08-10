4871d101fd7212d58047b7aa03f2eb817ab9ebe7
atharavmishra <68751866+atharavmishra@users.noreply.github.com>
2024-08-11 01:48:28 +0530

    initial commit

--- app/src/main/java/com/plcoding/meditationuiyoutube/ui/HomeScreen.kt
+++ app/src/main/java/com/plcoding/meditationuiyoutube/ui/HomeScreen.kt
@@ -7,18 +7,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.plcoding.meditationuiyoutube.R
@@ -32,7 +30,7 @@ fun HomeScreen() {
             .background(DeepBlue)
     ) {
         Column {
-
+            Greeting()
         }
     }
 
@@ -46,18 +44,19 @@ fun Greeting(
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 15.dp, end = 10.dp, start = 10.dp)
     ) {
-        Column(
-            verticalArrangement = Arrangement.Center
-        ) {
+        Column(verticalArrangement = Arrangement.Center) {
             Text(
                 text = "Good Morning $name",
                 style = MaterialTheme.typography.h2
             )
             Text(
-                text = "Good Morning $name",
-                style = MaterialTheme.typography.h2
+                text = "We wish you have a good day!",
+                style = MaterialTheme.typography.body1,
+                color = Color.White
             )
 
         }
@@ -67,6 +66,7 @@ fun Greeting(
             tint = Color.White,
             modifier = Modifier.size(25.dp)
         )
+
     }
 }
 
